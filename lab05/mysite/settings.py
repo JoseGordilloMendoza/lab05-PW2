5353ec2115478542fdbd42d017ab6aa2fa1479e0
Jose Gordillo Mendoza <jgordillome@unsa.edu.pe>
2023-06-13 18:33:53 -0500

    static root

--- lab05/mysite/settings.py
+++ lab05/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*cusg^!p86rf$49$!m^(i0^9uweqx&7(19h((c9o#85nj%07+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
